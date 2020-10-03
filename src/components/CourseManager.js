ea9baf7f41950aa158ff87253dc442612e4ceb07
Saleh Alkhalifa <salehesam@gmail.com>
2020-10-03 15:23:02 -0400

    Assignment 3 started, Components started, Services completed using the Postman with examples.

---
+++ src/components/CourseManager.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+
+export default class CourseManager extends React.Component {
+    render() {
+        return (
+                <div className="container-fluid">
+
+                </div>
+        );
+    }
+}
